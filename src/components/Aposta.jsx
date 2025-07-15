2221c0d5db24fde534fad26cfa049c46e781066c
Matheus Antonio Zucolotto Pereira <matheus.pereira@softplan.com.br>
2025-07-15 14:14:01 -0300

    feat(aposta): Refactor código componente aposta

--- src/components/Aposta.jsx
+++ src/components/Aposta.jsx
@@ -8,16 +8,16 @@ export default function Aposta({ values = [], handleChange, inputsRef }) {
             onChange={(e) => handleChange(idx, e)}
             maxLength={2}
             style={{
-            fontSize: '15px',
-            borderRadius: '15px',
-            padding: '4px',
-            width: '2ch',
-            border: '2px solid #2da84c',
-            textAlign: 'center',
-            margin: '4px',
-            boxShadow: '0 2px 5px #0d3316',
-            fontWeight: 'bold',
-            color: '#2da84c'
+                fontSize: '15px',
+                borderRadius: '15px',
+                padding: '4px',
+                width: '2ch',
+                border: '2px solid #2da84c',
+                textAlign: 'center',
+                margin: '4px',
+                boxShadow: '0 2px 5px #0d3316',
+                fontWeight: 'bold',
+                color: '#2da84c'
             }}
             type="number"
         />
@@ -25,7 +25,7 @@ export default function Aposta({ values = [], handleChange, inputsRef }) {
     return (
         <>
             <h1>Digite sua aposta</h1>
-            <div style={{ display: 'inline-flex', padding: '20px', flexDirection: 'column' }}>
+            <div style={{ display: 'inline-flex', padding: '10px', flexDirection: 'column' }}>
                 <div>{inputElements.slice(0, 6)}</div>
                 <div>{inputElements.slice(6)}</div>
             </div>
