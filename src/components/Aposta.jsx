8985c8fa9acf79278e854f1e25c777eab9d5b4fe
Matheus Antonio Zucolotto Pereira <matheus.pereira@softplan.com.br>
2025-07-15 13:18:33 -0300

    feat(layout): Ajusta layout apostas para 6-3

--- src/components/Aposta.jsx
+++ src/components/Aposta.jsx
@@ -1,26 +1,30 @@
-export default function Aposta({ values, handleChange, inputsRef }) {    
+export default function Aposta({ values, handleChange, inputsRef }) {
+    const inputElements = values.map((val, idx) => (
+        <input
+            key={idx}
+            id={idx}
+            value={val}
+            ref={(el) => (inputsRef.current[idx] = el)}
+            onChange={(e) => handleChange(idx, e)}
+            maxLength={2}
+            style={{
+            fontSize: '15px',
+            borderRadius: '15px',
+            padding: '4px',
+            width: '2ch',
+            border: '2px solid #000000af',
+            textAlign: 'center',
+            margin: '4px'
+            }}
+            type="number"
+        />
+    ));
     return (
         <>
             <h1>Digite sua aposta</h1>
-            <div style={{ display: 'inline-flex', gap: '5px', padding: '20px' }}>
-                {values?.map((val, idx) => (
-                <input
-                    id={idx}
-                    key={idx}
-                    value={val}
-                    ref={(el) => (inputsRef.current[idx] = el)}
-                    onChange={(e) => handleChange(idx, e)}
-                    maxLength={2}
-                    style={{
-                        fontSize: '15px',
-                        borderRadius: '15px',
-                        padding: '4px',
-                        width: '2ch',                        
-                        border: '2px solid #000000af',
-                        textAlign: 'center'
-                    }}
-                    type="number"
-                />))}
+            <div style={{ display: 'inline-flex', padding: '20px', flexDirection: 'column' }}>
+                <div>{inputElements.slice(0, 6)}</div>
+                <div>{inputElements.slice(6)}</div>
             </div>
         </>
     )
